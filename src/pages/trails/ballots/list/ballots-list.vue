dee89eea0b1f89276e72f24b01f15dc903908bec
Don Peat <don@caleos.io>
2022-08-25 13:07:19 -0500

    refactor: use src alias instead of relative paths

--- src/pages/trails/ballots/list/ballots-list.vue
+++ src/pages/trails/ballots/list/ballots-list.vue
@@ -1,15 +1,21 @@
 <script>
-import { mapActions, mapGetters, mapMutations } from 'vuex'
-import BallotForm from '../components/ballot-form'
-import BallotListItem from '../components/ballot-list-item'
-import BallotView from '../view/ballot-view'
-import WelcomeCard from '../../../../components/welcome-card'
-import ActionBar from '../../../../components/action-bar'
+import { mapActions, mapGetters, mapMutations } from "vuex";
+import BallotForm from "~/pages/trails/ballots/components/ballot-form";
+import BallotListItem from "~/pages/trails/ballots/components/ballot-list-item";
+import BallotView from "~/pages/trails/ballots/view/ballot-view";
+import WelcomeCard from "~/components/welcome-card";
+import ActionBar from "~/components/action-bar";
 
 export default {
-  name: 'ballots-list',
-  components: { BallotForm, BallotListItem, BallotView, WelcomeCard, ActionBar },
-  data () {
+  name: "ballots-list",
+  components: {
+    BallotForm,
+    BallotListItem,
+    BallotView,
+    WelcomeCard,
+    ActionBar,
+  },
+  data() {
     return {
       show: false,
       showBallot: false,
@@ -17,193 +23,229 @@ export default {
       timeAtMount: undefined,
       openedBallot: {},
       voting: false,
-      treasury: '',
+      treasury: "",
       statuses: [],
       categories: [],
       isBallotListRowDirection: true,
       currentPage: 1,
       limit: 30,
       page: 1,
-      sortMode: ''
-    }
+      sortMode: "",
+    };
   },
   props: {
     activeFilter: {
-      type: String
-    }
+      type: String,
+    },
   },
-  async mounted () {
-    console.log(`mounted`)
-    this.timeAtMount = Date.now()
-    this.statusChange = false
+  async mounted() {
+    console.log(`mounted`);
+    this.timeAtMount = Date.now();
+    this.statusChange = false;
     if (this.$route.params.id) {
-      this.showBallot = true
+      this.showBallot = true;
     }
     if (this.$route.query) {
-      this.treasury = this.$route.query.treasury
+      this.treasury = this.$route.query.treasury;
     }
-    this.resetBallots()
+    this.resetBallots();
 
-    console.log(`after reset ballots`)
-    await this.fetchFees()
-    this.$refs.infiniteScroll.reset()
-    this.$refs.infiniteScroll.poll()
+    console.log(`after reset ballots`);
+    await this.fetchFees();
+    this.$refs.infiniteScroll.reset();
+    this.$refs.infiniteScroll.poll();
   },
   methods: {
-    ...mapActions('trails', ['fetchFees', 'fetchBallots', 'castVote', 'fetchTreasuries', 'fetchBallotsByStatus']),
-    ...mapMutations('trails', ['resetBallots', 'stopAddBallots']),
+    ...mapActions("trails", [
+      "fetchFees",
+      "fetchBallots",
+      "castVote",
+      "fetchTreasuries",
+      "fetchBallotsByStatus",
+    ]),
+    ...mapMutations("trails", ["resetBallots", "stopAddBallots"]),
 
-    async onLoad (index, done) {
+    async onLoad(index, done) {
       if (!this.ballotsLoaded) {
-        this.limit += 50
+        this.limit += 50;
         const filter = {
           index: 4,
-          lower: this.treasury || (this.$route.query && this.$route.query.treasury),
-          upper: this.treasury || (this.$route.query && this.$route.query.treasury),
-          limit: this.limit
-        }
-        await this.fetchBallots(filter)
-        done()
+          lower:
+            this.treasury || (this.$route.query && this.$route.query.treasury),
+          upper:
+            this.treasury || (this.$route.query && this.$route.query.treasury),
+          limit: this.limit,
+        };
+        await this.fetchBallots(filter);
+        done();
       } else {
-        this.$refs.infiniteScroll.stop()
+        this.$refs.infiniteScroll.stop();
       }
     },
-    openBallotForm () {
-      this.show = true
+    openBallotForm() {
+      this.show = true;
     },
-    openBallot (ballot) {
+    openBallot(ballot) {
       if (this.showBallot) {
-        console.log('closing ballot', ballot)
-        this.showBallot = false
-        return
+        console.log("closing ballot", ballot);
+        this.showBallot = false;
+        return;
       }
-      console.log('opening ballot', ballot)
-      this.timeAtMount = Date.now()
-      this.$router.push(`/trails/ballots/${ballot.ballot_name}/${this.timeAtMount}`)
+      console.log("opening ballot", ballot);
+      this.timeAtMount = Date.now();
+      this.$router.push(
+        `/trails/ballots/${ballot.ballot_name}/${this.timeAtMount}`
+      );
       // the timestamp prevents scroll glitches on the infinite list
     },
-    getLocalTime (isoDate) {
-      const msOffset = new Date().getTimezoneOffset() * 60 * 1000
-      return new Date(isoDate).getTime() - msOffset
+    getLocalTime(isoDate) {
+      const msOffset = new Date().getTimezoneOffset() * 60 * 1000;
+      return new Date(isoDate).getTime() - msOffset;
     },
-    isBallotOpened (ballot) {
-      let endTime = this.getLocalTime(ballot.end_time)
-      let notExpired = endTime > Date.now()
-      let startTime = this.getLocalTime(ballot.begin_time)
-      let isStarted = startTime < Date.now()
-      return notExpired && isStarted
+    isBallotOpened(ballot) {
+      let endTime = this.getLocalTime(ballot.end_time);
+      let notExpired = endTime > Date.now();
+      let startTime = this.getLocalTime(ballot.begin_time);
+      let isStarted = startTime < Date.now();
+      return notExpired && isStarted;
     },
-    isBallotNotStarted (ballot) {
-      const startTime = this.getLocalTime(ballot.begin_time)
-      return startTime > Date.now()
+    isBallotNotStarted(ballot) {
+      const startTime = this.getLocalTime(ballot.begin_time);
+      return startTime > Date.now();
     },
 
-    displayWinner (ballot) {
-      if (!ballot.total_voters) return false
-      let winnerValue = -1
-      let winner
-      ballot.options.forEach(option => {
+    displayWinner(ballot) {
+      if (!ballot.total_voters) return false;
+      let winnerValue = -1;
+      let winner;
+      ballot.options.forEach((option) => {
         if (parseFloat(option.value) > winnerValue) {
-          winnerValue = parseFloat(option.value)
-          winner = option.key
+          winnerValue = parseFloat(option.value);
+          winner = option.key;
         }
-      })
-      return winner
+      });
+      return winner;
     },
-    votingHasBegun (ballot) {
-      let startTime = new Date(ballot.begin_time).getTime()
-      let isStarted = startTime < Date.now()
-      return isStarted
+    votingHasBegun(ballot) {
+      let startTime = new Date(ballot.begin_time).getTime();
+      let isStarted = startTime < Date.now();
+      return isStarted;
     },
-    getStartTime (ballot) {
-      return this.getLocalTime(ballot.begin_time)
+    getStartTime(ballot) {
+      return this.getLocalTime(ballot.begin_time);
     },
-    getEndTime (ballot) {
-      return this.getLocalTime(ballot.end_time)
+    getEndTime(ballot) {
+      return this.getLocalTime(ballot.end_time);
     },
-    isNewUser () {
-      return localStorage.isNewUser
+    isNewUser() {
+      return localStorage.isNewUser;
     },
-    updateTreasury (newTreasury) {
-      this.treasury = newTreasury
+    updateTreasury(newTreasury) {
+      this.treasury = newTreasury;
     },
-    updateStatuses (newStatuses) {
-      this.statuses = newStatuses
+    updateStatuses(newStatuses) {
+      this.statuses = newStatuses;
     },
-    updateCategories (newCategories) {
-      this.categories = newCategories
+    updateCategories(newCategories) {
+      this.categories = newCategories;
     },
-    filterBallots (ballots) {
-      const ballotFiltered = ballots.filter(b => {
+    filterBallots(ballots) {
+      const ballotFiltered = ballots.filter((b) => {
         if (this.statuses) {
           if (this.statuses.length === 0) {
-            return true
-          } else if (this.statuses.includes('active') && this.statuses.includes('expired')) {
-            return this.statuses.includes(b.status) || b.status === 'voting'
-          } else if (this.statuses.includes('active')) {
-            return this.statuses.includes(b.status) || (this.isBallotOpened(b) && b.status === 'voting')
-          } else if (this.statuses.includes('expired')) {
-            return this.statuses.includes(b.status) || (!this.isBallotOpened(b) && this.votingHasBegun(b) && b.status === 'voting')
-          } else if (this.statuses.includes('not started')) {
-            return this.statuses.includes(b.status) || (this.isBallotNotStarted(b) && b.status === 'voting')
+            return true;
+          } else if (
+            this.statuses.includes("active") &&
+            this.statuses.includes("expired")
+          ) {
+            return this.statuses.includes(b.status) || b.status === "voting";
+          } else if (this.statuses.includes("active")) {
+            return (
+              this.statuses.includes(b.status) ||
+              (this.isBallotOpened(b) && b.status === "voting")
+            );
+          } else if (this.statuses.includes("expired")) {
+            return (
+              this.statuses.includes(b.status) ||
+              (!this.isBallotOpened(b) &&
+                this.votingHasBegun(b) &&
+                b.status === "voting")
+            );
+          } else if (this.statuses.includes("not started")) {
+            return (
+              this.statuses.includes(b.status) ||
+              (this.isBallotNotStarted(b) && b.status === "voting")
+            );
           }
-          return this.statuses.includes(b.status)
+          return this.statuses.includes(b.status);
         }
-      })
-      return ballotFiltered.filter(b => this.categories.length === 0 || this.categories.includes(b.category))
+      });
+      return ballotFiltered.filter(
+        (b) =>
+          this.categories.length === 0 || this.categories.includes(b.category)
+      );
     },
-    changeDirection (isBallotListRowDirection) {
-      this.isBallotListRowDirection = isBallotListRowDirection
+    changeDirection(isBallotListRowDirection) {
+      this.isBallotListRowDirection = isBallotListRowDirection;
     },
-    getLoser () {
-      if (!this.ballot.total_voters || this.ballot.options.length !== 2) return false
-      return this.ballot.options.find(x => x.key !== this.getWinner.key)
+    getLoser() {
+      if (!this.ballot.total_voters || this.ballot.options.length !== 2)
+        return false;
+      return this.ballot.options.find((x) => x.key !== this.getWinner.key);
     },
-    sortBallots (ballots, method) {
-      if (method === 'A-Z') {
-        return ballots.sort((a, b) => a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1)
-      } else if (method === 'Z-A') {
-        return ballots.sort((a, b) => a.title.toLowerCase() < b.title.toLowerCase() ? 1 : -1)
-      } else if (method === 'Most popular') {
-        return ballots.sort((a, b) => a.total_voters < b.total_voters ? 1 : -1)
-      } else if (method === 'Least popular') {
-        return ballots.sort((a, b) => a.total_voters > b.total_voters ? 1 : -1)
-      } else if (method === '') {
-        return ballots
+    sortBallots(ballots, method) {
+      if (method === "A-Z") {
+        return ballots.sort((a, b) =>
+          a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1
+        );
+      } else if (method === "Z-A") {
+        return ballots.sort((a, b) =>
+          a.title.toLowerCase() < b.title.toLowerCase() ? 1 : -1
+        );
+      } else if (method === "Most popular") {
+        return ballots.sort((a, b) =>
+          a.total_voters < b.total_voters ? 1 : -1
+        );
+      } else if (method === "Least popular") {
+        return ballots.sort((a, b) =>
+          a.total_voters > b.total_voters ? 1 : -1
+        );
+      } else if (method === "") {
+        return ballots;
       }
     },
 
-    changeSortOption (option) {
-      this.sortMode = option
+    changeSortOption(option) {
+      this.sortMode = option;
     },
-    ballotContentImg (ballot) {
+    ballotContentImg(ballot) {
       try {
-        return JSON.parse(ballot.content).imageUrl
+        return JSON.parse(ballot.content).imageUrl;
       } catch (error) {
-        return null
+        return null;
       }
     },
-    updateCards (params) {
-      this.treasury = params.treasury
-      this.statuses = params.tatus
-      this.categories = params.type
-    }
+    updateCards(params) {
+      this.treasury = params.treasury;
+      this.statuses = params.tatus;
+      this.categories = params.type;
+    },
   },
   computed: {
-    ...mapGetters('accounts', ['isAuthenticated']),
-    ...mapGetters('trails', ['ballots', 'ballotsLoaded', 'treasuriesOptions'])
+    ...mapGetters("accounts", ["isAuthenticated"]),
+    ...mapGetters("trails", ["ballots", "ballotsLoaded", "treasuriesOptions"]),
   },
   watch: {
-    '$route' (to, from) {
-      console.log(`watching $route`)
+    $route(to, from) {
+      console.log(`watching $route`);
       if (to.params.id !== undefined) {
-        this.showBallot = true
+        this.showBallot = true;
       } else {
-        this.showBallot = false
+        this.showBallot = false;
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <template lang="pug">
