bedbc9ccf1e2f342f6f07f3b25828e93ef40b8ac
Artem Nuss <artem.nuss@megadevllc.com>
2022-09-07 20:20:16 +0200

    rename all components to same style

---
+++ src/pages/profiles/read/MyProfile.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+profile-detail(owner=true)
+</template>
+
+<script>
+import ProfileDetail from "~/pages/profiles/read/ProfileDetail";
+import { mapActions } from "vuex";
+export default {
+  name: "MyProfile",
+  components: { ProfileDetail },
+  methods: {
+    ...mapActions("profiles", ["getProfile"]),
+  },
+};
+</script>
