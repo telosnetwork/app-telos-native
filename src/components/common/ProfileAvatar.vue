940bfe8e9a963f202141a805b020c1fe68663c19
kylanhurt <kylan.hurt@gmail.com>
2022-09-17 22:11:41 +0100

    Reformat ProfileAvatar file

--- src/components/common/ProfileAvatar.vue
+++ src/components/common/ProfileAvatar.vue
@@ -6,51 +6,53 @@
       </q-avatar>
     </div>
     <div v-else>
-      <v-gravatar v-bind:hash="this.hash" :style="styleClass" class="q-avatar" />
+      <v-gravatar
+        v-bind:hash="this.hash"
+        :style="styleClass"
+        class="q-avatar"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import { GET_TABLE_ROWS } from '../../pages/resolve/util/fetch'
-import Gravatar from 'vue-gravatar'
-import md5 from 'md5'
+import { GET_TABLE_ROWS } from "../../pages/resolve/util/fetch";
+import md5 from "md5";
 
 export default {
-  props: ['account_name', 'size', 'childClass'],
-  components: {
-    'v-gravatar': Gravatar
-  },
-  data () {
+  props: ["account_name", "size", "childClass"],
+  data() {
     return {
-      avatar: '',
+      avatar: "",
       hash: md5(this.account_name),
-      styleClass: this.size ? { ...this.childClass, height: this.size, width: this.size } : this.childClass
-    }
+      styleClass: this.size
+        ? { ...this.childClass, height: this.size, width: this.size }
+        : this.childClass,
+    };
   },
-  async beforeMount () {
+  async beforeMount() {
     try {
       const { rows } = await GET_TABLE_ROWS({
-        code: 'profiles',
-        scope: 'profiles',
-        table: 'profiles',
+        code: "profiles",
+        scope: "profiles",
+        table: "profiles",
         json: true,
         limit: 1,
         lower_bound: this.account_name,
-        upper_bound: this.account_name
-      })
-      const [ profile ] = rows
+        upper_bound: this.account_name,
+      });
+      const [profile] = rows;
       if (profile) {
-        const { avatar } = profile
+        const { avatar } = profile;
         if (avatar) {
-          this.avatar = avatar
+          this.avatar = avatar;
         }
       }
     } catch (err) {
-      console.log('err: ', err)
+      console.log("err: ", err);
     }
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
