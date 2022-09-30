22741847146fc60dcf86b8e60fcbbfdc5d92f4f9
Artem Nuss <artem.nuss@megadevllc.com>
2022-10-01 00:56:48 +0200

    update

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,14 +1,13 @@
-import { mount } from '@vue/test-utils'
-import MessageComponent from '../../Test.vue'
-
-test('displays message', () => {
+import { mount } from "@vue/test-utils";
+import MessageComponent from "~/tests/unit/Test";
+test("displays message", () => {
   // mount() returns a wrapped Vue component we can interact with
   const wrapper = mount(MessageComponent, {
     propsData: {
-      msg: 'Hello world'
-    }
-  })
+      msg: "Hello world",
+    },
+  });
 
   // Assert the rendered text of the component
-  expect(wrapper.text()).toContain('Hello world')
-})
+  expect(wrapper.text()).toContain("Hello world");
+});
