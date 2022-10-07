8ceddbaff010bc6de9961436387c6e7567e39338
kylanhurt <kylan.hurt@gmail.com>
2022-10-07 20:08:04 +0100

    Prettier format cases page

--- src/pages/resolve/pages/cases/Cases.vue
+++ src/pages/resolve/pages/cases/Cases.vue
@@ -1,26 +1,24 @@
 <template>
-	<div>
-		<intro-card
-			heading="Create a new case"
-			text="Do you have an asset related issue that you’d like the network’s arbitration system to resolve? We believe that a transparent process benefits everyone."
-			buttonText="Create a new case"
-		/>
-		<case-files-table />
-	</div>
+  <div>
+    <intro-card
+      heading="Create a new case"
+      text="Do you have an asset related issue that you’d like the network’s arbitration system to resolve? We believe that a transparent process benefits everyone."
+      buttonText="Create a new case"
+    />
+    <case-files-table />
+  </div>
 </template>
 
 <script>
-import IntroCard from '../../components/IntroCard.vue'
-import CaseFilesTable from '../../components/CaseFilesTable.vue'
+import IntroCard from "../../components/IntroCard.vue";
+import CaseFilesTable from "../../components/CaseFilesTable.vue";
 
 export default {
   components: {
     IntroCard,
-    CaseFilesTable
-  }
-}
+    CaseFilesTable,
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
