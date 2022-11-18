0b5e72878f01ce7f058f54d9b56abfd4b12f1661
Viterbo <viter.rod@gmail.com>
2022-11-17 21:13:11 -0300

    changing the button "take me to my profile" to "sign in" after account creation

--- src/i18n/en-us/index.js
+++ src/i18n/en-us/index.js
@@ -113,6 +113,7 @@ export default {
                 buttons: {
                     continue: 'Continue',
                     goToProfile: 'Take me to my profile',
+                    signIn: 'Sign in',
                     verify: 'Verify Phone Number',
                     genKeys: 'Generate Keys',
                 },
--- src/pages/accounts/add/CongratulationsPage.vue
+++ src/pages/accounts/add/CongratulationsPage.vue
@@ -20,10 +20,10 @@ q-page.flex.align-center.column.q-pa-lg.congratulations
   .col-3
     q-btn.full-width(
       color="primary"
-      :label="$t('pages.accounts.add.buttons.goToProfile')"
+      :label="$t('pages.accounts.add.buttons.signIn')"
       size="lg"
       unelevated
-      to="/"
+      to="/login/?returnUrl=/"
     )
 </template>
 
