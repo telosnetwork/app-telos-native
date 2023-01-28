12c3ab18011da00dc960cdbabfb82ab805ff717e
Kylan Hurt <kylan@ridian.io>
2023-01-28 16:19:56 -0600

    Fix some error logging syntax

--- src/components/common/TelosProfileAvatar.vue
+++ src/components/common/TelosProfileAvatar.vue
@@ -36,7 +36,7 @@ export default {
     },
     methods: {
         onError() {
-            console.log('there is error');
+            console.log('Telos profile image load error');
             this.isError = true;
         },
     },
