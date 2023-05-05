7fa398b9f765f193e2b8cba8466b59acc91edc83
Karyne Mayer <karyne@detroitledger.tech>
2023-05-05 13:41:34 -0700

    Enabling eol

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,88 +1,90 @@
-const { resolve } = require('path');
+const { resolve } = require("path");
 module.exports = {
-  // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
-  // This option interrupts the configuration hierarchy at this file
-  // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-    es6: true
-  },
-  // https://eslint.vuejs.org/user-guide/#how-to-use-custom-parser
-  // Must use parserOptions instead of 'parser' to allow vue-eslint-parser to keep working
-  // `parser: 'vue-eslint-parser'` is already included with any 'plugin:vue/**' config and should be omitted
-  parserOptions: {
-    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/parser#configuration
-    // https://github.com/TypeStrong/fork-ts-checker-webpack-plugin#eslint
-    // Needed to make the parser take into account 'vue' files
-    extraFileExtensions: ['.vue'],
-    parser: '@typescript-eslint/parser',
-    project: resolve(__dirname, './tsconfig.json'),
-    tsconfigRootDir: __dirname,
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module' // Allows for the use of imports
-  },
+    // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
+    // This option interrupts the configuration hierarchy at this file
+    // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    // https://eslint.vuejs.org/user-guide/#how-to-use-custom-parser
+    // Must use parserOptions instead of 'parser' to allow vue-eslint-parser to keep working
+    // `parser: 'vue-eslint-parser'` is already included with any 'plugin:vue/**' config and should be omitted
+    parserOptions: {
+        // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/parser#configuration
+        // https://github.com/TypeStrong/fork-ts-checker-webpack-plugin#eslint
+        // Needed to make the parser take into account 'vue' files
+        extraFileExtensions: [".vue"],
+        parser: "@typescript-eslint/parser",
+        project: resolve(__dirname, "./tsconfig.json"),
+        tsconfigRootDir: __dirname,
+        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+        sourceType: "module", // Allows for the use of imports
+    },
 
-  // Rules order is important, please avoid shuffling them
-  extends: [
-    // Base ESLint recommended rules
-    'eslint:recommended',
-    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#usage
-    // ESLint typescript rules
-      // 'plugin:@typescript-eslint/recommended',
-    // consider disabling this class of rules if linting takes too long
-   //  'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    // Rules order is important, please avoid shuffling them
+    extends: [
+        // Base ESLint recommended rules
+        "eslint:recommended",
+        // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#usage
+        // ESLint typescript rules
+        // 'plugin:@typescript-eslint/recommended',
+        // consider disabling this class of rules if linting takes too long
+        //  'plugin:@typescript-eslint/recommended-requiring-type-checking',
 
-    // Uncomment any of the lines below to choose desired strictness,
-    // but leave only one uncommented!
-    // See https://eslint.vuejs.org/rules/#available-rules
-    'plugin:vue/vue3-essential', // Priority A: Essential (Error Prevention)
-    //'plugin:vue/vue3-strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
-    //'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
+        // Uncomment any of the lines below to choose desired strictness,
+        // but leave only one uncommented!
+        // See https://eslint.vuejs.org/rules/#available-rules
+        "plugin:vue/vue3-essential", // Priority A: Essential (Error Prevention)
+        //'plugin:vue/vue3-strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
+        //'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
 
-    // https://github.com/prettier/eslint-config-prettier#installation
-    // usage with Prettier, provided by 'eslint-config-prettier'.
-    // 'prettier'
-  ],
+        // https://github.com/prettier/eslint-config-prettier#installation
+        // usage with Prettier, provided by 'eslint-config-prettier'.
+        // 'prettier'
+    ],
 
-  plugins: ['vue'],
+    plugins: ["vue"],
 
+    globals: {
+        ga: "readonly", // Google Analytics
+        cordova: "readonly",
+        __statics: "readonly",
+        __QUASAR_SSR__: "readonly",
+        __QUASAR_SSR_SERVER__: "readonly",
+        __QUASAR_SSR_CLIENT__: "readonly",
+        __QUASAR_SSR_PWA__: "readonly",
+        process: "readonly",
+        Capacitor: "readonly",
+        chrome: "readonly",
+    },
 
-  globals: {
-    ga: 'readonly', // Google Analytics
-    cordova: 'readonly',
-    __statics: 'readonly',
-    __QUASAR_SSR__: 'readonly',
-    __QUASAR_SSR_SERVER__: 'readonly',
-    __QUASAR_SSR_CLIENT__: 'readonly',
-    __QUASAR_SSR_PWA__: 'readonly',
-    process: 'readonly',
-    Capacitor: 'readonly',
-    chrome: 'readonly'
-  },
-
-  // add your custom rules here
-  rules: {
-    'prefer-promise-reject-errors': 'off',
-    'no-prototype-builtins': 'off',
-    'no-useless-escape': 'off',
-    ' vue/multi-word-component-names': 'off',
-    'no-empty-pattern': 'off',
-    // TypeScript
-    quotes: ['warn', 'single', { avoidEscape: true }],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    indent: ['error', 4],
-    'max-len': ['warn', { 'code': 120, 'tabWidth': 2 }],
-    'no-trailing-spaces' : ['error', { 'skipBlankLines': true }],
-    eqeqeq: ['error', 'always'],
-    semi: ['error', 'always'],
-    'eol-last': ['error', 'never'],
-    'vue/component-tags-order': ['error', {
-      'order': [ [ 'script', 'template' ], 'style' ]
-    }],
-// allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
-}
+    // add your custom rules here
+    rules: {
+        "prefer-promise-reject-errors": "off",
+        "no-prototype-builtins": "off",
+        "no-useless-escape": "off",
+        " vue/multi-word-component-names": "off",
+        "no-empty-pattern": "off",
+        // TypeScript
+        quotes: ["warn", "single", { avoidEscape: true }],
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        indent: ["error", 4],
+        "max-len": ["warn", { code: 120, tabWidth: 2 }],
+        "no-trailing-spaces": ["error", { skipBlankLines: true }],
+        eqeqeq: ["error", "always"],
+        semi: ["error", "always"],
+        "eol-last": ["error", "always"],
+        "vue/component-tags-order": [
+            "error",
+            {
+                order: [["script", "template"], "style"],
+            },
+        ],
+        // allow debugger during development only
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    },
+};
