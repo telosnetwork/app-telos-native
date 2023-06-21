ae8cdd56f36f7ecd4b9b1a8ff26bb01af9e5114a
Don Peat <don@caleos.io>
2023-06-21 02:14:43 -0500

    fix: close main loading indicator on direct navigation

--- src/pages/governance/GovernanceDocumentation.vue
+++ src/pages/governance/GovernanceDocumentation.vue
@@ -1,25 +1,34 @@
 <script setup lang="ts">
-import { useQuasar } from 'quasar';
-import { onMounted, ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { getGovernanceHistory, GovernanceData } from './governanceHistory';
-import { QMarkdown } from '@quasar/quasar-ui-qmarkdown';
+import { useQuasar } from "quasar";
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+import { getGovernanceHistory, GovernanceData } from "./governanceHistory";
+import { QMarkdown } from "@quasar/quasar-ui-qmarkdown";
 
 const router = useRouter();
 const $q = useQuasar();
+const loading = ref<boolean>(false);
 const governanceData = ref<Array<GovernanceData>>([]);
 const goToProfile = (user: string) => {
-    router.push({ path: `/profiles/display/${user}` });
+  router.push({ path: `/profiles/display/${user}` });
 };
 
 onMounted(async () => {
-    $q.loading.show();
-    governanceData.value = await getGovernanceHistory();
-    $q.loading.hide();
+  $q.loading.hide(); // hide main app loading indicator (triggered on direct navigation)
+  loading.value = true;
+  governanceData.value = await getGovernanceHistory();
+  loading.value = false;
 });
 </script>
 
 <template>
+  <q-spinner-dots
+    class="fixed-center"
+    color="primary"
+    name="dots"
+    size="60px"
+    v-if="loading"
+  ></q-spinner-dots>
   <div class="governance-container">
     <div v-for="(amendment, index) in governanceData" :key="index">
       <div class="governance-container__amended-by">
