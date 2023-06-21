5ef68839ba33dab89d9508e686073c25627ece7b
Don Peat <don@caleos.io>
2023-06-20 23:51:25 -0500

    feat: link to contributing account profile

--- src/pages/governance/GovernanceDocumentation.vue
+++ src/pages/governance/GovernanceDocumentation.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
 import { getGovernanceHistory, GovernanceData } from "./governanceHistory";
 import { QMarkdown } from "@quasar/quasar-ui-qmarkdown";
 
+const router = useRouter();
+
 const governanceData = ref<Array<GovernanceData>>([]);
+
+const goToProfile = (user: string) => {
+  router.push({ path: `/profiles/display/${user}` });
+};
 
 onMounted(async () => {
   governanceData.value = await getGovernanceHistory();
@@ -11,16 +18,19 @@ onMounted(async () => {
 </script>
 
 <template>
-  <ol>
+  <div class="governance-container">
     <div v-for="(amendment, index) in governanceData" :key="index">
       <div>
-        On {{ amendment.lastAmended }} {{ amendment.amendedBy }} modified
-        {{ amendment.sections.length }} sections:
+        On {{ amendment.lastAmended }}
+        <a class="q-markdown--link" @click="goToProfile(amendment.amendedBy)">
+          {{ amendment.amendedBy }}
+        </a>
+        modified {{ amendment.sections.length }} sections:
       </div>
-      <ol>
+      <div>
         <div v-for="section in amendment.sections" :key="section.hash">
           <q-markdown>{{ section.text }}</q-markdown>
-          <div class="c-numbered-list__number">
+          <div>
             <a
               :href="'https://ipfs.telos.net/ipfs/' + section.hash"
               target="_blank"
@@ -28,9 +38,18 @@ onMounted(async () => {
             >
           </div>
         </div>
-      </ol>
+      </div>
     </div>
-  </ol>
+  </div>
 </template>
 
 <style src="@quasar/quasar-ui-qmarkdown/dist/index.css"></style>
+<style lang="scss">
+.governance-container {
+  margin-top: 2rem;
+  a {
+    color: blue;
+    cursor: pointer;
+  }
+}
+</style>
